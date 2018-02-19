6bd29a596718743012305e69c5c7d2f1a949ddf4
Fakher Oueslati <oueslati.fakher@gmail.com>
2018-02-19 10:43:43 -0500

    Update initvm.sh

--- initvm.sh
+++ initvm.sh
@@ -15,7 +15,7 @@ then
   sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
   sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make -y
   sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y autoremove && sudo apt-get  -y autoclean
-  sudo apt-get -y install -f screen openssh-server openssh-client terminator vim git nmap google-chrome-stable curl ubuntu-make default-jdk tmux make gcc
+  sudo apt-get -y install -f screen openssh-server openssh-client terminator vim git nmap google-chrome-stable curl ubuntu-make default-jdk tmux make gcc docker
   git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
   source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
   wget https://raw.githubusercontent.com/ofa5er/dotfilles/master/.screenrc && mv .screenrc ~/.screenrc
@@ -26,7 +26,6 @@ then
   git config --global user.name "Fakher Oueslati"
   git config --global user.email oueslati.fakher@gmail.com
   git config --global credential.helper 'cache --timeout=360000'
-  umake ide visual-studio-code
   # Run docker without sudo
   sudo groupadd docker
   sudo gpasswd -a $USER docker
@@ -35,6 +34,9 @@ then
   sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/latest/docker-compose-$(uname -s)-$(uname -m)"
   sudo chmod +x /usr/local/bin/docker-compose 
   docker-compose -v 
+  
+  
+  umake ide visual-studio-code
 else
   echo "Other distribution"
 fi
