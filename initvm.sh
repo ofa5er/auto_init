2c4e16aebb17ffe3e4759f1c93db19a028bd5674
Fakher Oueslati <oueslati.fakher@gmail.com>
2018-02-21 12:07:15 -0500

    Update initvm.sh

--- initvm.sh
+++ initvm.sh
@@ -49,11 +49,10 @@ elif [ -f /etc/debian_version ]; then
 
     umake ide visual-studio-code
   elif [ "$dist" == "debian" ]; then
-    #Debian Jessie
+    #Debian Jessie --- Borken for now
     echo "Debian"
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
     sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-    sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make -y
     sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y autoremove && sudo apt-get  -y autoclean
     sudo apt-get -y install -f screen openssh-server openssh-client terminator vim git nmap google-chrome-stable curl default-jdk tmux make gcc zsh build-essential module-assistant
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
