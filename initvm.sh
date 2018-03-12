b880eb34e2c3510b9d24753d615c31c0b0a2f391
Fakher Oueslati <oueslati.fakher@gmail.com>
2018-03-12 08:41:50 -0400

    Update initvm.sh

--- initvm.sh
+++ initvm.sh
@@ -28,7 +28,7 @@ elif [ -f /etc/debian_version ]; then
     # Install Docker
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    sudo apt-get update 
+    sudo apt-get update
     apt-cache policy docker-ce 
     sudo apt-get install -y docker-ce 
     sudo systemctl status docker 
@@ -46,13 +46,14 @@ elif [ -f /etc/debian_version ]; then
   elif [ "$dist" == "debian" ]; then
     #Debian Jessie --- Borken for now
     echo "Debian"
+    sudo apt-get -y install -f curl apt-transport-https
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
     sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
     sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
     sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
     sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y autoremove && sudo apt-get  -y autoclean
-    sudo apt-get -y install -f screen openssh-server openssh-client terminator vim git nmap google-chrome-stable curl tmux make gcc zsh build-essential module-assistant code
+    sudo apt-get -y install -f screen openssh-server openssh-client terminator vim git nmap google-chrome-stable curl tmux make gcc zsh build-essential module-assistant
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
     source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
     wget https://raw.githubusercontent.com/ofa5er/dotfiles/master/.screenrc && mv .screenrc ~/.screenrc
